69a23c82bfab44be96903f7da5f08097f6997219
waelotman211 <waelotman211@gmail.com>
2022-12-27 17:37:13 +0200

    update app file

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,18 @@
-import React , {Component} from 'react';
+import React  from 'react';
 import {BrowserRouter , Route, Routes} from 'react-router-dom';
 import LogIn from './Component/LogIn';
 import Nav from './Component/Nav';
 import SignUp from './Component/signUp';
 
-class App extends Component{
+class App extends React.Component{
 render () {
   return (
     <BrowserRouter>
     <div className="app">
       <Nav />
       <Routes>
-      <Route path="/LogIn" component= {LogIn}/>
-      <Route path="/signUp" component= {SignUp}/>
+      <Route path="/LogIn" element= {<LogIn/>}></Route>
+      <Route path="/signUp" element= {<SignUp/>}/>
       </Routes>
     </div>
     </BrowserRouter> 
