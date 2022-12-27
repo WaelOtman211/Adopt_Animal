b59514fcf12f6d3ef05fa2accf60cc090b04a093
noor99 <nnosh.rhal.99@gmail.com>
2022-12-27 17:00:53 +0200

    add function login and sign up...

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {Component} from 'react';
+import {BrowserRouter , Route, Routes} from 'react-router-dom';
+import LogIn from './Component/LogIn';
+import Nav from './Component/Nav';
+import SignUp from './Component/signUp';
 
-function App() {
+class App extends Component{
+render () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="app">
+      <Nav />
+      <Routes>
+      <Route path="/LogIn" component= {LogIn}/>
+      <Route path="/signUp" component= {SignUp}/>
+      </Routes>
     </div>
+    </BrowserRouter> 
   );
+}
 }
 
 export default App;
---
+++ app/src/Component/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+const Nav = () =>{
+    return(
+        <div className="navbar">
+            <div className="container">
+            <a href="#" className="logo">PetsHome</a>
+            <ul>
+                <a href="/LogIn">LogIn</a>
+                <a href="/signUp">SignUp</a>
+            </ul>
+            </div>
+            
+        </div>
+    );
+
+}
+
+export default Nav;
