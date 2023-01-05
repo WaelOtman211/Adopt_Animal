ef06d2a099a68313fdfabcdf4eed371c7eda635c
skittan4 <souheilkittan@gmail.com>
2023-01-06 00:21:15 +0200

    DB connection with adopt history

--- database/init.sql
+++ database/init.sql
@@ -1,17 +1,17 @@
 BEGIN;
 
-DROP TABLE IF EXISTS user,Admin,password,myFavorite;
+DROP TABLE IF EXISTS users,Admin,password,myFavorite,adoptHistory;
 
   
 
-CREATE TABLE user (
+CREATE TABLE users(
   id VARCHAR(9) PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   username VARCHAR(255) NOT NULL,
   role VARCHAR(15) NOT NULL
 );
 
-CREATE TABLE password (
+CREATE TABLE password(
   id serial PRIMARY KEY,
   password  VARCHAR(255),
   userID VARCHAR(9) REFERENCES users(id)
@@ -25,14 +25,22 @@ animalPhoto text,
 animalName VARCHAR(255),
 animalSize VARCHAR(15),
 animalStatus VARCHAR(15)
+);
 
-
-)
-
+CREATE TABLE adoptHistory(
+id serial PRIMARY KEY,
+userID VARCHAR(9) REFERENCES users(id),
+animalID VARCHAR(15) not null, 
+animalPhoto text,
+animalName VARCHAR(255),
+animalSize VARCHAR(15),
+animalStatus VARCHAR(15),
+adoptDate date
+);
 
 INSERT INTO users (id,name,username,role) VALUES
   ('123456789','Admin', 'admin','admin');
  INSERT INTO password (password,userID) VALUES 
-  ('1234','123456789')
+  ('1234','123456789');
 
 COMMIT;
--- server/server.js
+++ server/server.js
@@ -6,19 +6,36 @@ const cors = require ('cors');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
+process.on('uncaughtException', function (err) {
+    console.log(err);
+}); 
 app.listen(4000,() => {console.log("server started on port http://localhost:4000")})
 
 
 app.post("/SignUp" , async(req, res) =>{   
     const {username, password}=req.body;
-    let data=await db.query(`INSERT INTO users (username, password) VALUES ($1,$2) RETURNING * `,[username, password]);
+    let data=await db.query(`INSERT INTO users (username, password) VALUES ('${username}','${password}') RETURNING * `);
+
         res.json(data.rows)
         
 
 });  
 
-    
+app.post("/adoptAnimal" , async(req, res) =>{   
+    const {animalID,date,userID,photo,animalName,size,status}=req.body;
+    console.log(req.body)
+    let data=await db.query(`INSERT INTO adopthistory (userID,animalID,animalPhoto,animalName,animalSize,animalStatus,adoptDate) VALUES ('${userID}','${animalID}','${photo}','${animalName}','${size}','${status}','${date}') RETURNING * `);
+        res.json(data.rows)
+});  
+
+
+app.post("/setfavorite" , async(req, res) =>{   
+    const {animalID,userID,photo,animalName,size,status}=req.body;
+    console.log(req.body)
+    let data=await db.query(`INSERT INTO myfavorite (userID,animalID,animalPhoto,animalName,animalSize,animalStatus) VALUES ('${userID}','${animalID}','${photo}','${animalName}','${size}','${status}') RETURNING * `);
+        res.json(data.rows)
+});  
+
 
 app.get('/LogIn', (req, res) =>{
     const username = req.body.username;
