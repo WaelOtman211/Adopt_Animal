2dec54b5720ed2943629a76dea3645432ab754db
noor99 <nnosh.rhal.99@gmail.com>
2023-01-06 19:31:11 +0200

    LogIn is Done

--- app/src/Component/LogIn.jsx
+++ app/src/Component/LogIn.jsx
@@ -1,67 +1,64 @@
-import React,{useState} from 'react';
-import "./styles/Login.css"
-import Axios from 'axios';
+import React, { useState } from "react";
+import "./styles/Login.css";
+import Axios from "axios";
 const LogIn = () => {
-   
-        
+  const [usernameLogin, setusernameLogin] = useState("");
+  const [passwordLogin, setpasswordLogin] = useState("");
+  const [loginstatus, setloginstatus] = useState("");
 
-    const [usernameLogin, setusernameLogin] = useState('')
-    const [passwordLogin, setpasswordLogin] = useState('')
-    const [loginstatus, setloginstatus] = useState('')
-
-     /* const submit = useEffect(() => {
-        Axios.get("http://localhost:4000/LogIn", { 
-            username: usernameLogin,
-            password: passwordLogin
-        }).then((response) => {
-            if(response.data.message){
-                setloginstatus(response.data.message);
-            }
-            else{
-                setloginstatus(response.data[0].username)
-            }
-         
-        });
-    })  */
-
-    const onSubmit = async (e) => {
-        e.preventDefault()
-        const get = {
-          username: usernameLogin,
-          password: passwordLogin
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const post = {
+      username: usernameLogin,
+      password: passwordLogin,
+    };
+    try {
+      const response = await Axios.post("http://localhost:4000/LogIn", post);
+      if (response.data.message) {
+        setloginstatus(response.data.message);
+      } else {
+        setloginstatus(response.data.username);
       }
-        try {
-          const response = await Axios.get('http://localhost:4000/LogIn', get);
-          if(response.data.message){
-            setloginstatus(response.data.message);
-        }
-        else{
-            setloginstatus(response.data[0].username)
-        }
-     }
-         catch (e) {
-          alert(e)
-        }
-      } 
-    
-    return (
-        <div className='login'>
-           
-                <label htmlFor="uname"><b>Username</b></label>
-                <input type="text" onChange={(e) => {
-                    setusernameLogin(e.target.value);
-                }} placeholder="Enter Username" name="uname" value={usernameLogin} required />
-                <br></br>
-                <label htmlFor="psw"><b>Password</b></label>
-                <input type="password" onChange={(e) => {
-                   setpasswordLogin(e.target.value);
-                }} placeholder="Enter Password" name="psw" value={passwordLogin} required />
+    } catch (e) {
+      alert(e);
+    }
+  };
 
-                <button onClick={onSubmit}>Login</button>
-                <h1>{loginstatus}</h1>
-        </div>
-    
+  return (
+    <div className="login">
+      <form>
+        <label htmlFor="username">
+          <b>Username</b>
+        </label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setusernameLogin(e.target.value);
+          }}
+          placeholder="Enter Username"
+          name="username"
+          value={usernameLogin}
+          required
+        />
+        <br></br>
+        <label htmlFor="password">
+          <b>Password</b>
+        </label>
+        <input
+          type="password"
+          onChange={(e) => {
+            setpasswordLogin(e.target.value);
+          }}
+          placeholder="Enter Password"
+          name="password"
+          value={passwordLogin}
+          required
+        />
 
-)
-            }
+        <button onClick={onSubmit}>Login</button>
+        <h6>{loginstatus}</h6>
+      </form>
+    </div>
+  );
+};
 export default LogIn;
--- server/server.js
+++ server/server.js
@@ -1,51 +1,50 @@
-const express = require ('express');
-const db = require ('./database/connections');
+const express = require("express");
+const db = require("./database/connections");
 const app = express();
-const cors = require ('cors');
+const cors = require("cors");
+const bcrypt = require("bcrypt");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(4000,() => {console.log("server started on port http://localhost:4000")})
-
-
-app.post("/SignUp" , async(req, res) =>{   
-    const {username, password}=req.body;
-    let data=await db.query(`INSERT INTO users (username, password) VALUES ($1,$2) RETURNING * `,[username, password]);
-        res.json(data.rows)
-        
-
-});  
-
-    
-
-app.get('/LogIn', (req, res) =>{
-    const username = req.body.username;
-    const password = req.body.password;
-    
-    db.query(
-        'SELECT * FROM users WHERE username = $1 AND password = $2',
-
-
-
-        
-        'SELECT * FROM users WHERE username = $ AND password = $',
-
-        [username, password],
-        (err, result) =>{
-           if (err){
-            res.send({err: err});
-           }
-           if(result){
-            res.send(result);
-           }
-           else{
-            res.send({message: "wrong username/password"});
-           }
-        }
-    );   
+app.listen(4000, () => {
+  console.log("server started on port http://localhost:4000");
 });
 
+const saltRounds = 10;
+app.post("/SignUp", async (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, saltRounds);
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+  let data = await db.query(
+    `INSERT INTO users (username, password) VALUES ($1,$2) RETURNING * `,
+    [username, hash]
+  );
+  res.json(data.rows);
+});
 
+app.post("/LogIn", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
+  db.query(
+    `SELECT * FROM users WHERE username = $1`,
+    [username],
+    async (err, result) => {
+      if (err) {
+        return res.json({ err: err });
+      } else if (result) {
+        //console.log(result);
+        let compare = await bcrypt.compare(password, result.rows[0].password);
+        console.log(compare);
+        if (compare) {
+          return res.json(result.rows[0]);
+        } else {
+          res.json({ message: "wrong username/password" });
+        }
+      }
+    }
+  );
+});
